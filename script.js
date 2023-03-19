67c5773b7038d348979b3a58c03996e7cf6b2f5c
josef <info@slezak-it.de>
2023-03-19 20:12:26 +0100

    init

---
+++ script.js
@@ -0,0 +1,6 @@
+const beispiel1 = document.querySelector("#beispiel1");
+beispiel1.addEventListener("click", executeExample);
+
+function executeExample() {
+  alert("das ist ein Beispiel für den Funtionsaufruf");
+}
