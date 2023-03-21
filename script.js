36c9e7836ce4e0e83fdf8ac2febde75e61470ebf
Tamara <tam.grimm.22@lehre.mosbach.dhbw.de>
2023-03-21 11:22:40 +0100

    erstes Beispiel aufrufen

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ const beispiel1 = document.querySelector("#beispiel1");
 beispiel1.addEventListener("click", executeExample);
 
 function executeExample() {
-  alert("das ist ein Beispiel für den Funtionsaufruf");
+  const str1 = 'A \'quote\' inside a string.';
+  const str2 = "Another \"quote\" inside a string.";
+  alert(`${str1}
+  ${str2}`);
 }
